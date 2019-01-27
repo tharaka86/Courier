124660f4b39dfca28b2bae3a1ebadfb0ccf683d4
kasun <tharaka86@gmail.com>
2019-01-27 16:49:08 +0000

    initial checkin

---
+++ FundApps.Courier/FundApps.Courier/Parcel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundApps.Courier
+{
+    public class Parcel
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+    }
+}
